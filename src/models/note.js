334ef1cb6eee81e8dc8d8e1d4f89961f9994141d
Victoria Minets <anni.nicht@gmail.com>
2025-11-09 12:15:52 +0200

    add homework

---
+++ src/models/note.js
@@ -0,0 +1,42 @@
+// src/models/note.js
+
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true, // прибирає пробіли на початку та в кінці
+    },
+    content: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    tag: {
+      type: String,
+      required: false,
+      default: 'Todo',
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -1,87 +1,37 @@
 // src/server.js
+
 import express from 'express';
-import cors from 'cors';
-import pino from 'pino-http';
 import 'dotenv/config'; // запускає скрипт, який витягує змінні з .env в корені додатка. розширений варіант dotenv.config ({шлях}) - якщо десь в іншому місці треба брати змінні оточення
+import cors from 'cors';
 import helmet from 'helmet';
 
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import notesRoutes from './routes/notesRoutes.js';
+
 const app = express();
-// Використовуємо значення з .env або дефолтний порт 3000
-const PORT = process.env.PORT ?? 3000; // process глобальна змінна (як window в js)
+const PORT = process.env.PORT ?? 3000; // Використовуємо значення з .env або дефолтний порт 3000
 
-// Middleware
-
-app.use(cors()); // Дозволяє запити з будь-яких джерел
-
+// Глобальні middleware
+app.use(logger); // 1. Логер першим — бачить усі запити
+app.use(express.json({ limit: '100kb' })); // 2. Парсинг JSON у body запиту
+app.use(cors()); // 3. Дозвіл для запитів з інших доменів
 app.use(helmet()); // для безпеки від шкідливих запитів - дуже рекомендовано
 
-app.use(express.json()); // Middleware для парсингу JSON у body запиту
+// підключаємо групу маршрутів
+app.use(notesRoutes);
 
-// Логування HTTP-запитів
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+// 404 — якщо маршрут не знайдено
+app.use(notFoundHandler);
 
-// --- Додаткове логування часу ---
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
+// Error — якщо під час запиту виникла помилка
+app.use(errorHandler);
 
-// Кореневий маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, NoteHub!' });
-});
+await connectMongoDB();
 
-// --- Маршрут для отримання всіх нотаток ---
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-// --- Маршрут для отримання нотатки за ID ---
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-// Маршрут для тестування middleware помилки
-app.get('/test-error', (req, res) => {
-  // Штучна помилка для прикладу
-  throw new Error('Simulated server error');
-});
-
-// Middleware 404 (після всіх маршрутів)
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// Middleware для обробки помилок сервера 500 (останнє)
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
-});
-
-// --- Запуск сервера ---
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
