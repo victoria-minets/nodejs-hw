0d9e45d726401d1f7beab490085b5e01fc79a75b
Victoria Minets <anni.nicht@gmail.com>
2025-11-30 16:35:28 +0200

    improve sendMail name

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,7 @@ import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 
 import jwt from 'jsonwebtoken';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 import handlebars from 'handlebars';
 import path from 'node:path';
